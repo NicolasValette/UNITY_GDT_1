b45a6bbfede6f16bc86fee47bac1c2d5a8cfb254
NikoMyoji <nicolas.vlte@gmail.com>
2024-07-09 15:20:08 +0200

    Update fire method
    enable to hold fire button

--- Assets/Scripts/Inputs/Fire.cs
+++ Assets/Scripts/Inputs/Fire.cs
@@ -9,7 +9,7 @@ namespace GDT1.Inputs
     public class Fire : MonoBehaviour
     {
         [SerializeField]
-        private float _cooldown;
+        private float _cooldown = 0.1f;
         [SerializeField]
         private GameObject _bulletPrefab;
         [SerializeField]
@@ -19,6 +19,8 @@ namespace GDT1.Inputs
         [SerializeField]
         private float _bulletLifeTime = 2f;
 
+        private bool _isFirePressed = false;
+        private float _timeSinceLastShoot = 0f;
         // Start is called before the first frame update
         void Start()
         {
@@ -28,16 +30,36 @@ namespace GDT1.Inputs
         // Update is called once per frame
         void Update()
         {
-        
+            if (_isFirePressed )
+            {
+                _timeSinceLastShoot += Time.deltaTime;
+                if (_timeSinceLastShoot > _cooldown)
+                {
+                    ShootBullet();
+                }
+            }
         }
 
         public void OnFire(InputValue value)
         {
-            Debug.Log("press : " + value.isPressed);
-            
+            if (!_isFirePressed)
+            {
+                _isFirePressed = true;
+                ShootBullet();
+            }
+            else
+            {
+                _isFirePressed = false;
+            }
+        }
+
+        private void ShootBullet()
+        {
+
             GameObject bullet = Instantiate(_bulletPrefab, _bulletSpawnPos.position, gameObject.transform.rotation);
             bullet.GetComponent<Rigidbody2D>().AddForce(gameObject.transform.up * _bulletSpeed, ForceMode2D.Impulse);
             Destroy(bullet, _bulletLifeTime);
+            _timeSinceLastShoot = 0;
         }
     }
 }
